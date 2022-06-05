d0e6e9e26fc85cbb358912496e730d5426f3679f
Swamiappan Puthurajan <ozfosaami@gmail.com>
2022-06-05 08:29:32 +0800

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+if 5 > 2:
+    print("5 is greater than 2") #Output will be "5 is greater than 2", this is single line comment.
+"""
+This is an exapmple for multi ine ccooment. 
+This is an exapmple for multi ine ccooment. 
+This is an exapmple for multi ine ccooment. 
+This is an exapmple for multi ine ccooment. 
+"""
+x = 90
+print (x)
+print (type(x))
+x = "five"
+print (x)
+print (type(x))
